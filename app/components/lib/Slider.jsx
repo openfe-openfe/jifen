b0799b233b558afbffe5dee235db2a08a75285e7
sonhao <15376364250@163.com>
2016-12-05 16:12:07 +0800

    轮播图改用rap

--- app/components/lib/Slider.jsx
+++ app/components/lib/Slider.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
 import Slider from 'react-slick'
-
+import request from '../commen/request.js'
 export default React.createClass({
+  getInitialState() {
+    return {
+      lists: [],
+      loading:false
+    }
+  },
+  componentWillMount(){
+    this.setState({
+      loading:true
+    })
+  },
+  componentDidMount() {
+       this.fetchFn()
+  },
+  // 获取数据
+  fetchFn(){
+    var that=this
+    request.get('http://rap.taobao.org/mockjsdata/7918/songhao/headlines')
+      .then((data) => {
+        console.log(data)
+        this.setState(
+          {
+            lists:data.data,
+            loading:false
+          }
+        )
+       })
+      .catch((e) => { console.log(e.message) })
+  },
   render: function () {
     var settings = {
         infinite: true,
@@ -10,12 +39,19 @@ export default React.createClass({
         slidesToScroll: 1,
         arrows: false,
         autoplay: true
-    };
+    }
     return (
         <Slider {...settings}>
-          <div><img src={require('../../img/eat.jpg')} className="HomeBanner-image-2T6G"/></div>
-          <div><img src={require('../../img/network.jpg')} className="HomeBanner-image-2T6G"/></div>
-          <div><img src={require('../../img/skip.jpg')} className="HomeBanner-image-2T6G"/></div>
+          {
+
+            this.state.lists.map((e,index) => {
+                return (
+                  <div className="" key={index}>
+                    <div><img src={e.image_mobile} className="HomeBanner-image-2T6G"/></div>
+                  </div>
+                )
+            })
+          }
         </Slider>
     );
   }
